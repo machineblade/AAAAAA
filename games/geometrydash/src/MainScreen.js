56a56b4fb2c91e0680fafc7127748f01114c81e1
Machine_Blade <machine.blade2991@gmail.com>
2026-04-03 21:32:05 -0400

    test

--- games/geometrydash/src/MainScreen.js
+++ games/geometrydash/src/MainScreen.js
@@ -1,5 +1,6 @@
 import { Background } from './Background.js';
 import { Button } from './Button.js';
+import { bounceOut } from './easings.js';
 
 export class MainScene extends PIXI.Container {
 
@@ -22,6 +23,29 @@ export class MainScene extends PIXI.Container {
         });
         this.addChild(this.creator);
 
+        // ── Creator bounce state ──
+        this._creatorHeld = false;
+        this._creatorFrom = 1;
+        this._creatorTo = 1;
+        this._creatorDuration = 0;
+        this._creatorElapsed = 0;
+        this._creatorAnim = false;
+
+        this.creator.on('pointerdown', () => {
+            this._creatorHeld = true;
+            this._creatorBounce(1.26, 0.3);
+        });
+        this.creator.on('pointerupoutside', () => {
+            this._creatorHeld = false;
+            this._creatorBounce(1, 0.4);
+        });
+        this.creator.on('pointerover', () => {
+            if (this._creatorHeld) this._creatorBounce(1.26, 0.3);
+        });
+        this.creator.on('pointerout', () => {
+            if (this._creatorHeld) this._creatorBounce(1, 0.4);
+        });
+
         // ── Play button centred on screen ──
         this.playButton = new Button('playbutton', () => {
             console.log('Play pressed!');
@@ -38,15 +62,35 @@ export class MainScene extends PIXI.Container {
         this.gameName.scale.set(300 / this.gameName.texture.width);
         this.gameName.position.set(dw / 2, 12);
 
-        // Creator logo: 30 design points tall, 10px from bottom-left corner
+        // Creator logo: 20 design points tall, 10px from bottom-left corner
         this.creator.scale.set(20 / this.creator.texture.height);
         this.creator.position.set(10, dh - 10);
 
         this.playButton.position.set(dw / 2, dh / 2);
     }
 
+    _creatorBounce(toScale, duration) {
+        this._creatorFrom = this.creator.scale.x;
+        this._creatorTo = toScale * (20 / this.creator.texture.height);
+        this._creatorDuration = duration;
+        this._creatorElapsed = 0;
+        this._creatorAnim = true;
+    }
+
     tick(dt) {
         this.background.tick(dt);
         this.playButton.tick(dt);
+
+        if (this._creatorAnim) {
+            this._creatorElapsed += dt;
+            if (this._creatorElapsed >= this._creatorDuration) {
+                this._creatorElapsed = this._creatorDuration;
+                this._creatorAnim = false;
+            }
+            const t = this._creatorElapsed / this._creatorDuration;
+            const eased = bounceOut(t);
+            const scale = this._creatorFrom + (this._creatorTo - this._creatorFrom) * eased;
+            this.creator.scale.set(scale);
+        }
     }
 }
